7bb2b00dd4a8eba0a5126c2cc00b22fdcaaaa796
brendahuwitonze <uwitonzebrendah@gmail.com>
2024-02-22 11:01:14 +0200

    function to mark task complete

--- toDo.js
+++ toDo.js
@@ -2,33 +2,59 @@ let myList ={
 day1:{
 school:'study',
 work:'reports',
-home:'eat'
+home:'eat',
+completed:'false'
 },
 day2:{
     school:'presentation',
     work:'approving reports',
-    home:'laundary'
+    home:'laundary',
+    completed: 'true'
+},
+day3:{
+    school:'interneship period',
+    work:'Orientation of the new people',
+    home:'family time',
+    completed: 'true'
 }
-
 }
 // Function to add another day 
-const newDay=(newDay,school,work,home)=>{
+const newDay=(newDay,school,work,home,completed)=>{
     myList[newDay]={
         school:school,
         work:work,
-        home:home
+        home:home,
+        completed:completed
     }
+    console.log(myList);
         
 }
-//calling the function to add another day
-newDay('day3','work on project','give report','watch movie')
-console
-// console.log(myList);
+
+ newDay('day4','work on project','give report','watch movie','true')
+
+
 
 
 // Function to display a task by category
+
 function accessSpecificDay (newDay){
-let entries = Object.entries(newDay)
-console.log(entries)
+       for (const entry of Object.entries(newDay)){
+       
+console.log(`${entry[0]} => ${entry[1]}`)
+     }
+ }
+
+accessSpecificDay( myList.day3)
+
+
+//  function to mark tasks complete
+function completedDays(markeDayComplete){
+    if(myList[markeDayComplete] && myList[markeDayComplete].completed == 'true'){
+        myList[markeDayComplete].completed == 'true';
+        console.log(`Day ${markeDayComplete} marked as complete.`);
+    }
+    else{
+    console.log(`${markeDayComplete} is incomplete.`);
 }
-accessSpecificDay(myList.day3)
+}
+completedDays('Day2');
